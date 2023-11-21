5f2e6c03c03236d207002e0627d4c8e19231f7cd
SerafimovaD <daci.serafimova@gmail.com>
2023-11-21 12:52:10 +0100

    Add first java program

---
+++ src/proekt_2/Menuvacnica.java
@@ -0,0 +1,13 @@
+package proekt_2;
+
+public class Menuvacnica {
+
+	public static void main(String[] args) {
+		double x=100;
+		double euro=61.7;
+		double rezultat=x*euro;
+		System.out.print (rezultat);
+
+	}
+
+}
